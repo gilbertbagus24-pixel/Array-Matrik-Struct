dcee96d4deb25fa8c24f99858980bf62bc6535bd
Gilbert Bagus N <gilbertbagus24@gmail.com>
2026-03-20 22:15:29 +0700

    Add files via upload

---
+++ latiankuis.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct buku(){
+	int id, tahun ;
+	string nama, penulis;
+	}
+
+int main(){
+	cout << "==MENU==\n";
+	cout << "1. Tambah Buku \n";
+	cout << "2. Tampilkan Semua Buku \n";
+	cout << "3. Cari Buku berdasarkan ID \n";
+	cout << "4. Cari berdasarkan Judul \n";
+	}
