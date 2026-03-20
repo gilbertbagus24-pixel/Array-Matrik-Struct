f1a4459d01d3906da7fc0bc70dea59c10fc82ecd
Gilbert Bagus N <gilbertbagus24@gmail.com>
2026-03-20 23:02:55 +0700

    Refactor buku struct and add functionality
    Refactor struct definition and add functions for adding and displaying books.

--- latiankuis.cpp
+++ latiankuis.cpp
@@ -1,12 +1,55 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-struct buku(){
+struct buku{
 	int id, tahun ;
-	string nama, penulis;
+	string judul, penulis;
+	};
+
+buku dat[100];
+int jumlah = 0; 
+int autoID = 1;
+
+void tambahbuku(){
+	int banyak;
+	cout << "Masukan jumlah buku: ";
+	cin >> banyak;
+	cin .ignore();
+	for(int i=0; i<banyak; i++){
+		dat[jumlah].id = autoID++;
+		cout << "Judul buku: ";
+		getline(cin, dat[jumlah].judul);
+		cout << "Nama penulis: ";
+		getline(cin, dat[jumlah].penulis);
+		cout << "Tahun rilis buku: ";
+		cin >> dat[jumlah].tahun;
+		cin.ignore();
+		jumlah++;
+		
+		}
 	}
 
-int main(){
+void tampilbuku(){
+	cout << left;
+	cout << setw(8) << "ID";
+	cout << setw(30) << "Judul";
+	cout << setw(15) << "Penulis";
+	cout << setw(10) << "Tahun rilis";
+	cout << "\n---------------------------------------------------------------\n";
+	for(int i = 0; i<jumlah;
+	 i++){
+		cout << setw(8) << dat[i].id;
+		cout << setw(30) << dat[i].judul;
+		cout << setw(15) << dat[i].penulis;
+		cout << setw(10) << dat[i].tahun << endl;	
+		}
+	}
+	
+void caribuku(){
+	
+	}
+
 	int pilihan;
 	
 	do {
